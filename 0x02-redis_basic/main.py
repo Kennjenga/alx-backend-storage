b33be133ccea4815f85c15df8f3c28d0a556d7d9
Kennjenga <kinyagia10@gmail.com>
2025-02-05 14:48:05 +0300

    Add call history tracking and replay functionality to Cache methods

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -8,6 +8,21 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
+
+inputs = cache._redis.lrange(
+    "{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange(
+    "{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
+
 # TEST_CASES = {
 #     b"foo": None,
 #     123: int,
@@ -22,12 +37,14 @@ cache = Cache()
 # key = cache.store(data)
 # print(key)
 
-cache.store(b"first")
-print(cache.get(cache.store.__qualname__))
+# store functionality
+# cache.store(b"first")
+# print(cache.get(cache.store.__qualname__))
 
-cache.store(b"second")
-cache.store(b"third")
-print(cache.get(cache.store.__qualname__))
+
+# cache.store(b"second")
+# # cache.store(b"third")
+# print(cache.get(cache.store.__qualname__))
 
 
 local_redis = redis.Redis()
